c9dcf1d3d95654dfec676aed82c0485def032c50
alesandrobarbosa <barbosa.alesandro@gmail.com>
2020-12-08 00:59:35 -0300

    creating input search

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import InputSearch from "../InputSearch";
 
 class Header extends Component {
   render() {
     return (
       
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <InputSearch />
         </header>
       
     );
